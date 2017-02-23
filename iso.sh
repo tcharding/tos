5dd7d8e40e1c2ce992852c403efa9a9885b98e8f
tcharding <me@tobin.cc>
2017-02-23 14:36:13 +1100

    Configure grub timeout to 0

--- iso.sh
+++ iso.sh
@@ -8,6 +8,9 @@ mkdir -p isodir/boot/grub
 
 cp sysroot/boot/tos.kernel isodir/boot/tos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
+set timeout=0
+set default=0 # Set the default menu entry
+
 menuentry "tos" {
 	multiboot /boot/tos.kernel
 }
